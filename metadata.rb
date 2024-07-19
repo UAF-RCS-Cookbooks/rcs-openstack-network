398a2b3a3e1008b5f86778472052fadf6ebca337
Jeremiah Dabney <jdabney@gmail.com>
2024-07-18 17:22:57 -0800

    Converting to RCS managed

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'openstack-network'
-maintainer       'openstack-chef'
-maintainer_email 'openstack-discuss@lists.openstack.org'
+name             'rcs-openstack-network'
+maintainer       'UAF RCS'
+maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Installs and configures the OpenStack Network API Service and various agents and plugins'
 version          '20.0.0'
@@ -9,10 +9,8 @@ version          '20.0.0'
   supports os
 end
 
-depends 'openstackclient'
-depends 'openstack-common', '>= 20.0.0'
-depends 'openstack-identity', '>= 20.0.0'
+depends 'rcs-openstackclient'
+depends 'rcs-openstack-common', '>= 20.0.0'
+depends 'rcs-openstack-identity', '>= 20.0.0'
 
-issues_url 'https://launchpad.net/openstack-chef'
-source_url 'https://opendev.org/openstack/cookbook-openstack-network'
 chef_version '>= 16.0'

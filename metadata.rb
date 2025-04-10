f9974578e669fdb1162519b0271fd113828affc9
Jeremiah Dabney <jdabney@gmail.com>
2025-04-09 22:08:28 -0800

    Remove version constraints.

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Installs and configures the OpenStack Network API Service and various agents and plugins'
-version          '20.0.5'
+version          '20.0.6'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'rcs-openstackclient'
-depends 'rcs-openstack-common', '>= 20.0.0'
-depends 'rcs-openstack-identity', '>= 20.0.0'
+depends 'rcs-openstack-common'
+depends 'rcs-openstack-identity'
 
 chef_version '>= 16.0'
